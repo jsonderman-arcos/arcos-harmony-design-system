e6c78a286daf67a695d767d9de4be06594c63013
blue148 <jason@blue148.com>
2025-09-17 12:54:05 -0500

    muitheme import fix

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,3 +4,12 @@ export declare const coreFlat: any;
 export declare const themeMuiFlat: any;
 export declare const coreMuiFlat: any;
 
+declare const _default: {
+  muiThemeJson: any;
+  themeFlat: any;
+  coreFlat: any;
+  themeMuiFlat: any;
+  coreMuiFlat: any;
+};
+
+export default _default;
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@
 // Export generated JSON theme/token assets for consumers.
 
 // MUI theme options as JSON (use with MUI createTheme)
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - JSON module
-export { default as muiThemeJson } from './theme/mui-theme.json';
+import muiThemeJson from './theme/mui-theme.json';
 
 // Flat token JSONs
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - JSON module
-export { default as themeFlat } from './theme/theme.flat.json';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - JSON module
-export { default as coreFlat } from './theme/core.flat.json';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - JSON module
-export { default as themeMuiFlat } from './theme/theme.muiflat.json';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - JSON module
-export { default as coreMuiFlat } from './theme/core.muiflat.json';
+import themeFlat from './theme/theme.flat.json';
+import coreFlat from './theme/core.flat.json';
+import themeMuiFlat from './theme/theme.muiflat.json';
+import coreMuiFlat from './theme/core.muiflat.json';
+
+export { muiThemeJson, themeFlat, coreFlat, themeMuiFlat, coreMuiFlat };
+
+export default {
+  muiThemeJson,
+  themeFlat,
+  coreFlat,
+  themeMuiFlat,
+  coreMuiFlat,
+};
 
 // Note: CSS token files are included in the package under `theme/`.
 // Consumers can import them directly via their bundler if desired.
